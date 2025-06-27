daaf58120835239bfc20a87991238e25062d066f
Aryanka Shende <aryankashende2006@gmail.com>
2025-06-27 15:15:34 +0000

    Updated add expenses

--- project.py
+++ project.py
@@ -8,11 +8,14 @@ def main():
 
 def add_expense():
     print("Successfully in add_expense")
+    category = input("Enter Category: ").strip()
+    expense = int(input("Expense: "))
+    print(f"Expense added!!!\n{category}:Rs.{expense}")
 
 
 def expense_chart():
     print("Successfully in expense_chart")
-    
+
 
 if __name__ == "__main__":
     main()
