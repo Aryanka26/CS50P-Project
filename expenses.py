2a479159dedb959f7df69ee32a5684d89bac1e91
Aryanka Shende <aryankashende2006@gmail.com>
2025-07-09 14:47:59 +0000

    update

--- expenses.py
+++ expenses.py
@@ -1,5 +1,6 @@
 import csv
 
+
 class Expenses:
     @classmethod
     def get_my_expense(cls):
@@ -8,20 +9,27 @@ class Expenses:
             reader = csv.reader(file)
             for row in reader:
                 my_expense.append(row)
-        return my_expense 
+        return my_expense
 
     @classmethod
     def write_my_expense(cls, my_expense):
-        with open("my_expense.csv", 'w') as file:
+        with open("my_expense.csv", "w") as file:
             writer = csv.writer(file)
-            writer.writerow(['Category','Your Expense'])
+            writer.writerow(["Category", "Your Expense"])
             for row in my_expense:
                 writer.writerow(row)
 
     @classmethod
     def clear_my_expense(cls):
-        cleared_expenses = [['Category', 'Your Expense'], ['Food', '0'], ['Stationary', '0'], ['Travel', '0'], ['Entertainment', '0', ], ['Gifts', '0']]
-        with open("my_expense.csv", 'w') as file:
+        cleared_expenses = [
+            ["Category", "Your Expense"],
+            ["Food", "0"],
+            ["Stationary", "0"],
+            ["Travel", "0"],
+            ["Entertainment", "0"],
+            ["Gifts", "0"]
+        ]
+        with open("my_expense.csv", "w") as file:
             writer = csv.writer(file)
             for row in cleared_expenses:
                 writer.writerow(row)
